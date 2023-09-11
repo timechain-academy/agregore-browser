f183392f186ff9ea7732179ca7a202216cd5bfbf
randymcmillan <randy.lee.mcmillan@gmail.com>
2023-09-11 18:29:55 -0400

    gnostr.icon:artwork

---
+++ app/version.js
@@ -0,0 +1,31 @@
+
+export const name = 'agregore-browser'
+export const version = '2.2.3'
+export const dependencies = {
+  "abort-controller": "^3.0.0",
+  "bt-fetch": "^3.1.1",
+  "create-desktop-shortcuts": "^1.7.0",
+  "data-uri-to-buffer": "^3.0.1",
+  "decompress": "^4.2.1",
+  "electron-extended-webextensions": "0.0.10",
+  "fetch": "1.1.0",
+  "fs-extra": "^9.0.1",
+  "gemini-fetch": "^2.0.0",
+  "gemini-to-html": "^1.0.0",
+  "go-ipfs": "^0.17.0",
+  "hyper-sdk": "^4.2.5",
+  "hypercore-fetch": "^9.3.1",
+  "ipfs-http-client": "^60.0.0",
+  "ipfsd-ctl": "^13.0.0",
+  "js-ipfs-fetch": "^5.0.0",
+  "mime": "^2.4.6",
+  "multiformats": "^9.9.0",
+  "node-pre-gyp": "0.17.0",
+  "rc": "^1.2.8",
+  "sanitize-filename": "^1.6.3",
+  "scoped-fs": "^1.4.1",
+  "semver": "^7.5.0",
+  "ssb-fetch": "^1.5.2",
+  "whatwg-mimetype": "https://github.com/jsdom/whatwg-mimetype#v2.3.0",
+  "wrtc": "0.4.7"
+}
